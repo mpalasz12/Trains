a62ff53397148d7e7a0846b2b334ef0b8f71f061
bnowak1 <01178569@pw.edu.pl>
2024-05-16 10:54:11 +0200

    name changes

--- src/main/java/com/bd2/bd2project/Bd2projectApplication.java
+++ src/main/java/com/bd2/bd2project/Bd2projectApplication.java
@@ -1,13 +1,13 @@
-package com.bd2projekt.BD2Projekt;
+package com.bd2.bd2project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Bd2ProjektApplication {
+public class Bd2projectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Bd2ProjektApplication.class, args);
+		SpringApplication.run(Bd2projectApplication.class, args);
 	}
 
 }
--- src/test/java/com/bd2/bd2project/Bd2projectApplicationTests.java
+++ src/test/java/com/bd2/bd2project/Bd2projectApplicationTests.java
@@ -1,10 +1,10 @@
-package com.bd2projekt.BD2Projekt;
+package com.bd2.bd2project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Bd2ProjektApplicationTests {
+class Bd2projectApplicationTests {
 
 	@Test
 	void contextLoads() {
