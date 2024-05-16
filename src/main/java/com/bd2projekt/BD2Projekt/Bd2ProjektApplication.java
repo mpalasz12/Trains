f2399d8dec1808f8ccada3e828c767cd7b940427
bnowak1 <01178569@pw.edu.pl>
2024-05-16 10:47:45 +0200

    empty project structure

---
+++ src/main/java/com/bd2projekt/BD2Projekt/Bd2ProjektApplication.java
@@ -0,0 +1,13 @@
+package com.bd2projekt.BD2Projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bd2ProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bd2ProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bd2projekt/BD2Projekt/Bd2ProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd2projekt.BD2Projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bd2ProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
