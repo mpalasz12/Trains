8179dcd34c2af1de2507347daee4d9c3b1a727f0
Szymon S <01178812@pw.edu.pl>
2024-06-24 00:42:19 +0200

    stationManager.js

--- front_merge/src/Canvas/Logic/StationsManager.js
+++ front_merge/src/Canvas/Logic/StationsManager.js
@@ -1,21 +1,38 @@
 import Station from "./Station";
 
+export function addCity(city) {
+	try {
+		const params = new URLSearchParams();
+		params.append("name", city);
+		const response = axios.post('http://localhost:8080/data/add_city?' + params.toString());
+		console.log('Answer from server: ', response.data);
+	} catch (error) {
+		console.error("Error adding locomotive: ", error);
+	}
+}
+
 class StationsManager
 {
-    constructor()
-    {
-        this.stations = [
-            new Station("Gdańsk", 230, 65),
-            new Station("Kołobrzeg", 90, 75),
-            new Station("Białystok", 432, 165),
-            new Station("Warszawa", 340, 230),
-            new Station("Poznań", 145, 210),
-            new Station("Łódź", 270, 270),
-            new Station("Wrocław", 150, 320),
-            new Station("Kraków", 300, 402),
-            new Station("Rzeszów", 400, 395)
-        ];
-    }
+  constructor()
+  {
+      this.stations = [
+          new Station("Gdańsk", 230, 65),
+          new Station("Kołobrzeg", 90, 75),
+          new Station("Białystok", 432, 165),
+          new Station("Warszawa", 340, 230),
+          new Station("Poznań", 145, 210),
+          new Station("Łódź", 270, 270),
+          new Station("Wrocław", 150, 320),
+          new Station("Kraków", 300, 402),
+          new Station("Rzeszów", 400, 395)
+      ];
+  }
+  
+  initCities() {
+    this.stations.forEach(station => {
+      addCity(station.name);
+    });
+  }
 
     findStationByName(name) {
         for (const station of this.stations) {
